89778f9d0419eda3e64cf7ed7e780a0eb64c27ea
bin <l.binray@hotmail.com>
2020-02-27 20:25:33 +0800

    stl:use 'find()' algorithm

--- stl/basic_stl.cpp
+++ stl/basic_stl.cpp
@@ -10,10 +10,14 @@ using namespace std;
 
 // 迭代器使用
 void use_iterator();
+// find() 算法使用
+void use_find();
 
 int main(int argc, char* argv[]){
-    use_iterator();
+//    use_iterator();
+    use_find();
 }
+
 
 void use_iterator(){
     // 定义vector
@@ -26,7 +30,7 @@ void use_iterator(){
 
     // 定义迭代器
     vector<int>::iterator iter;             //常量 const_iterator
-    for(iter = v.begin(); iter != v.end(); iter++){
+    for(iter = v.begin(); iter != v.end(); ++iter){ //++前置比后置速度快
         cout << *iter << " ";
         *iter *= 2;
     }
@@ -34,10 +38,25 @@ void use_iterator(){
 
     // 定义反向迭代器
     vector<int>::reverse_iterator iter_r;   //常量 const_reverse_iterator
-    for(iter_r = v.rbegin(); iter_r != v.rend(); iter_r++){
+    for(iter_r = v.rbegin(); iter_r != v.rend(); ++iter_r){
         cout << *iter_r << " ";
     }
     cout << endl;
 
+}
+
+void use_find(){
+    vector<int> v;
+    for(int i = 0; i < 5; i++){
+        v.push_back(i);
+    }
+    vector<int>::const_iterator iter;
+//    iter = find(v.begin(), v.end(), 5);
+    iter = find(v.begin(), v.end(), 3);
+    if(iter != v.end()){
+        cout << "exist:" << *iter << endl;
+    }else{
+        cout << "not exist" << endl;
+    }
 }
 
