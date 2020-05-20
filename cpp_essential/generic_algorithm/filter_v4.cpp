11e38595951c6521f3ffa5757144de45c6a44d70
knight <l.binray@hotmail.com>
2020-05-20 09:27:55 +0800

    [cpp_essential]feat:import "insertion adapter" to transform assignment into push_back(), and we don't need to initialize the capacity of the vector.

--- cpp_essential/generic_algorithm/filter_v4.cpp
+++ cpp_essential/generic_algorithm/filter_v4.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <iterator>     // std::back_inserter
 
 using namespace std;
 
@@ -26,9 +27,13 @@ int main( int argc, char** argv){
     const int elem_size = 8;
     int ia[ elem_size ] = { 12, 8, 43, 0, 6, 21, 3, 7};
     vector<int> ivec( ia, ia+8 );
-    vector<int> nvec( 8 );
 
-    filter( ivec.begin(), ivec.end(), nvec.begin(), 10, less<int>() );
+    //vector<int> nvec( 8 );
+    //filter( ivec.begin(), ivec.end(), nvec.begin(), 10, less<int>() );
+
+    // import "insertion adapter" to transform assignment into push_back(), and we don't need to initialize the capacity of the vector.
+    vector<int> nvec;
+    filter( ivec.begin(), ivec.end(), back_inserter(nvec), 10, less<int>());
 
     for ( int i = 0; i < nvec.size(); i++){
         cout << nvec[i] << endl;
