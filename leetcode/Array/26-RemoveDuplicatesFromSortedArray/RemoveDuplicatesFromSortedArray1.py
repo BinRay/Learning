20beed790591a868949116c344855c334efb5a3b
knight <l.binray@hotmail.com>
2020-05-27 12:05:09 +0800

    [leetcode]feat: 26-RemoveDuplicatesFromSortedArray, python version

--- leetcode/Array/26-RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray1.py
+++ leetcode/Array/26-RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray1.py
@@ -1,7 +1,22 @@
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
+    def removeDuplicates(self, nums):
+        if len(nums) == 0:
+            return 0
+        
+        base = 1
+        explorer = 1
+        length = len(nums)
+
+        while explorer < length:
+            if nums[ explorer ] != nums[ explorer-1 ]:
+                nums[ base ] = nums [ explorer ]
+                base+=1
+            explorer+=1
+        
+        return base
 
 
 nums = [1, 2, 3, 3, 3, 4, 4, 5, 70, 70, 72, 75, 100, 200, 200, 300]
 s = Solution()
-s.removeDuplicates()
+s.removeDuplicates( nums )
+print( nums )
