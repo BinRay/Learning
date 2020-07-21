3da0fab6861e7964bdaf8fbd2b6ef4fd88054e9f
knight <l.binray@hotmail.com>
2020-07-21 11:48:34 +0800

    feat: quick sort

--- algorithm/sort/Sort.cpp
+++ algorithm/sort/Sort.cpp
@@ -6,6 +6,7 @@ using namespace std;
 class MergeSort{
 public:
     void merge_sort( vector<int> & nums ){
+        cout << "merge sort: " << endl;
         vector<int> temp( nums );
         divide( nums, temp, 0, temp.size() - 1 );
     }
@@ -48,13 +49,52 @@ public:
     }
 };
 
+class QuickSort{
+public:
+    void quick_sort( vector<int> &nums ){
+        cout << " quick sort:" << endl;
+        quick_sort( nums, 0, nums.size() - 1 );
+    }
 
+    void quick_sort( vector<int> &nums, int first, int last ){
+        int index = partition( nums, first, last );
+
+        if ( first < index -1 ) {
+            quick_sort( nums, first, index -1 );
+        }
+        if ( index + 1 < last  )  {
+            quick_sort( nums, index + 1, last );
+        }
+    }
+
+    int partition( vector<int> &nums, int first, int last ){
+        int pivot = nums[first];
+        int left = first + 1;
+        int right = last;
+
+        while ( left < right ){
+            while ( nums[left] <= pivot ) left++;
+            while ( nums[right] > pivot ) right--;
+
+            if ( left < right ){
+                swap( nums[left], nums[right] );
+            }
+        }
+
+        swap( nums[first], nums[right]);
+        return right;
+
+    }
+};
 int main( int argc, char** argv ){
 
     vector<int> nums = { 3, 2, 4, 1, 19, -1, 0 ,2 ,77, 54, 108, 99, 22, 88};
 
     MergeSort m;
-    m.merge_sort( nums );
+    //m.merge_sort( nums );
+
+    QuickSort s;
+    s.quick_sort( nums );
 
     for ( auto i: nums ){
         cout << i << " ";
