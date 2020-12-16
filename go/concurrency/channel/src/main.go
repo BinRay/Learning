f7a276e841207dba280624aeb46be9734750daf4
binliu <binliu@zego.im>
2020-12-16 12:01:25 +0800

    feat: go-concurrency-chan

---
+++ go/concurrency/channel/src/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main(){
+	// 构建一个通道
+	ch := make(chan int)
+
+	// 开启一个并发匿名函数
+	go func() {
+
+		for i := 3; i >= 0; i-- {
+			ch <- i
+			time.Sleep(time.Second)
+		}
+	}()
+
+	for data := range ch{
+		fmt.Println(data)
+
+		if data == 0 {
+			break
+		}
+	}
+}
